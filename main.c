a473c4218dd31abde8f3c621de1a05ca3cfb6d7e
Samuel Hym <samuel@tarides.com>
2025-01-29 19:31:22 +0100

    Display argc and argv

--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
-#include <unistd.h>
 
-int main() {
-  puts("main started\n");
+int main(int argc, char *argv[]) {
+  int i;
+  printf("argc: %d\n", argc);
+  for (i = 0; i < argc; i++)
+    printf("argv[%d]: %s\n", i, argv[i]);
   fflush(stdout);
   return 0;
 }
