3fd0499d8c6ae43082bb0e09470f440f3b3fc20b
Samuel Hym <samuel@tarides.com>
2025-04-03 15:01:38 +0200

    Simplify the code

--- main.c
+++ main.c
@@ -1,34 +1,17 @@
-#include <stdio.h>
 #include <pthread.h>
 #include <assert.h>
 
 int main() {
   pthread_mutex_t m;
   pthread_mutexattr_t attr;
-  int r;
 
-  printf("Hello Unikraft\n");
-  fflush(stdout);
-
-  r = pthread_mutexattr_init(&attr);
-  assert(r == 0);
-  r = pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
-  assert(r == 0);
-  r = pthread_mutex_init(&m, &attr);
-  assert(r == 0);
+  pthread_mutexattr_init(&attr);
+  pthread_mutexattr_settype(&attr, PTHREAD_MUTEX_ERRORCHECK);
+  pthread_mutex_init(&m, &attr);
   pthread_mutexattr_destroy(&attr);
 
-  r = pthread_mutex_lock(&m);
-  assert(r == 0);
-
-  printf("With mutex!\n");
-  fflush(stdout);
-
-  r = pthread_mutex_unlock(&m);
-  assert(r == 0);
-
-  printf("Without mutex!\n");
-  fflush(stdout);
+  pthread_mutex_lock(&m);
+  assert(0); // ^ Previous line triggered a segfault
 
   return 0;
 }
